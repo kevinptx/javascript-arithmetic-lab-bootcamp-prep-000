14e60a6cc48d00d9c90c1584b0aa9d6d52aa68c2
Kevin Peery <kevin.peery@gmail.com>
2018-03-17 02:40:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ function subtract(a, b){
   return a - b
 }
 
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a/b
+}
+
+function inc(n){
+  return n++
+}
+
 function makeInt(n){
   return parseInt(n, 10)
 }
